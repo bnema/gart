e58e7a07beba0eafad3e28e71fefb7746089119d
bnema <hi@bnema.dev>
2024-03-03 05:19:32 +0100

    First draw

---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
+)
+
+type model struct {
+	watchedFolder string
+	cacheFolder   string
+	fileName      string
+	lastModified  map[string]time.Time
+}
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "gart",
+		Short: "Gart surveille vos fichiers de configuration et copie les changements dans un dossier cache",
+	}
+
+	var watchCmd = &cobra.Command{
+		Use:   "watch [folder] -n [name]",
+		Short: "Surveille un dossier de configuration",
+		Args:  cobra.ExactArgs(1),
+		Run:   watchFolder,
+	}
+
+	watchCmd.Flags().StringP("name", "n", "", "Nom de la configuration à surveiller")
+
+	rootCmd.AddCommand(watchCmd)
+	rootCmd.Execute()
+}
+
+func watchFolder(cmd *cobra.Command, args []string) {
+	folder := args[0]
+	name, _ := cmd.Flags().GetString("name")
+
+	initialModel := model{
+		watchedFolder: folder,
+		cacheFolder:   "./cache", // Vous pourriez vouloir configurer ce chemin
+		fileName:      name,
+		lastModified:  make(map[string]time.Time),
+	}
+
+	p := tea.NewProgram(initialModel)
+	_, err := p.Run()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return m.watchFiles
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+	case tea.KeyMsg:
+		return m, tea.Quit
+	case tea.WindowSizeMsg:
+		return m, nil
+	}
+
+	return m, m.watchFiles
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("Vous surveillez le dossier '%s' pour les fichiers '%s'", m.watchedFolder, m.fileName)
+}
+
+func (m model) watchFiles() tea.Msg {
+	files, err := os.ReadDir(m.watchedFolder)
+	if err != nil {
+		log.Printf("Erreur lors de la lecture du dossier: %v", err)
+		return nil
+	}
+
+	for _, file := range files {
+		filePath := filepath.Join(m.watchedFolder, file.Name())
+		if fileChanged(filePath, m.lastModified[file.Name()]) {
+			destPath := filepath.Join(m.cacheFolder, file.Name())
+			err := copyFile(filePath, destPath)
+			if err != nil {
+				log.Printf("Erreur lors de la copie du fichier: %v", err)
+			} else {
+				m.lastModified[file.Name()] = time.Now()
+			}
+		}
+	}
+	return nil
+}
+
+func copyFile(src, dst string) error {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s n'est pas un fichier standard", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+	_, err = io.Copy(destination, source)
+	return err
+}
+
+func fileChanged(path string, lastModTime time.Time) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return info.ModTime().After(lastModTime)
+}
