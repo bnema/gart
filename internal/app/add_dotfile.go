c2b1f3bf201602815153ae571972b1468e7f1969
Bamen <hi@bamen.dev>
2024-08-08 12:01:28 +0200

    Refacto: Fix config.toml being replaced for each new dotfile added. Segment the AddDotfile function into smaller functions for each logical step

--- internal/app/add_dotfile.go
+++ internal/app/add_dotfile.go
@@ -13,132 +13,137 @@ import (
 )
 
 func (app *App) AddDotfile(path, name string) {
+	app.mu.Lock()
+	defer app.mu.Unlock()
 
-	// If the path starts with ~, replace it with the user's home directory
+	path = app.expandHomeDir(path)
+
+	if strings.HasSuffix(path, "*") {
+		app.addMultipleDotfiles(path, name)
+	} else {
+		app.addSingleDotfile(path, name)
+	}
+}
+
+// expandHomeDir replaces the ~ in a path with the user's home directory
+func (app *App) expandHomeDir(path string) string {
 	if strings.HasPrefix(path, "~") {
 		home, err := os.UserHomeDir()
 		if err != nil {
 			fmt.Printf("Error getting home directory: %v\n", err)
-			return
+			return path
 		}
-		path = home + path[1:]
+		return home + path[1:]
+	}
+	return path
+}
+
+// addMultipleDotfiles adds multiple dotfiles to the storage folder
+func (app *App) addMultipleDotfiles(path, name string) {
+	directoryPath := strings.TrimSuffix(path, "*")
+	dirs, err := filepath.Glob(filepath.Join(directoryPath, ".*"))
+	if err != nil {
+		fmt.Printf("Error using filepath.Glob: %v\n", err)
+		return
 	}
 
-	if strings.HasSuffix(path, "*") {
-		directoryPath := strings.TrimSuffix(path, "*")
+	dirChan := make(chan string, len(dirs))
+	app.populateDirChannel(dirs, dirChan)
 
-		// Use filepath.Glob to get the list of directories matching the pattern
-		dirs, err := filepath.Glob(filepath.Join(directoryPath, ".*"))
-		if err != nil {
-			fmt.Printf("Error using filepath.Glob: %v\n", err)
-			return
-		}
-
-		// Create a wait group to wait for all worker goroutines to finish
-		var wg sync.WaitGroup
-
-		// Determine the number of worker goroutines based on available CPU cores
-		numWorkers := runtime.NumCPU()
-
-		// Create a buffered channel to hold the directory paths
-		dirChan := make(chan string, len(dirs))
-
-		// Send the directory paths to the channel
-		for _, dir := range dirs {
-			if (filepath.Base(dir) == ".config" && strings.Contains(dir, "gart")) || filepath.Base(dir) == ".local" {
-				fmt.Printf("Ignored directory: %s\n", dir)
-				continue
-			}
-			if info, err := os.Stat(dir); err == nil && info.IsDir() {
-				dirChan <- dir
-			}
-		}
-		close(dirChan)
-
-		// Start the worker goroutines
-		for i := 0; i < numWorkers; i++ {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				for dirPath := range dirChan {
-					err := filepath.Walk(dirPath, func(filePath string, info os.FileInfo, err error) error {
-						if err != nil {
-							return err
-						}
-
-						// Check if the file size is within the limit (1024*1024 bytes)
-						if !info.IsDir() && info.Size() > 1024*1024 {
-							fmt.Printf("Ignored file due to size limit: %s\n", filePath)
-							return nil
-						}
-
-						relPath, _ := filepath.Rel(directoryPath, filePath)
-						destPath := filepath.Join(app.StorePath, name, relPath)
-
-						if info.IsDir() {
-							err := os.MkdirAll(destPath, info.Mode())
-							if err != nil {
-								return err
-							}
-						} else {
-							err := system.CopyFile(filePath, destPath)
-							if err != nil {
-								return err
-							}
-						}
-
-						return nil
-					})
-
-					if err != nil {
-					}
-
-					fmt.Printf("Dotfiles added: %s\n", name)
-
-				}
-			}()
-		}
-
-		// Wait for all worker goroutines to finish
-		wg.Wait()
-
-		// Start a new goroutine to save the configuration
-		go func() {
-			err := config.SaveConfig(app.ConfigFilePath, *app.Config)
-			if err != nil {
-				fmt.Printf("Error saving configuration: %v\n", err)
-				return
-			}
-			fmt.Printf("Configuration saved successfully\n")
-		}()
-
-	} else {
-
-		cleanedPath := filepath.Clean(path)
-		fmt.Printf("Adding dotfile: %s\n", cleanedPath)
-
-		storePath := filepath.Join(app.StorePath, name)
-		fmt.Printf("Store path: %s\n", storePath)
-
-		err := system.CopyDirectory(cleanedPath, storePath)
-		if err != nil {
-			fmt.Printf("Error copying directory: %v\n", err)
-			return
-		}
-
-		// Add the dotfile to the map of dotfiles
-		if app.Config.Dotfiles == nil {
-			app.Config.Dotfiles = make(map[string]string)
-		}
-		app.Config.Dotfiles[name] = cleanedPath
-
-		// Save the updated configuration
-		err = config.SaveConfig(app.ConfigFilePath, *app.Config)
-		if err != nil {
-			fmt.Printf("Error saving configuration: %v\n", err)
-			return
-		}
-
-		fmt.Printf("Dotfile added: %s\n", name)
+	var wg sync.WaitGroup
+	numWorkers := runtime.NumCPU()
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go app.workerCopyFiles(dirChan, &wg, directoryPath, name)
 	}
+	wg.Wait()
+
+	app.updateConfig(name, path)
+}
+
+// populateDirChannel populates the directory channel with directories
+func (app *App) populateDirChannel(dirs []string, dirChan chan<- string) {
+	for _, dir := range dirs {
+		if app.shouldIgnoreDir(dir) {
+			fmt.Printf("Ignored directory: %s\n", dir)
+			continue
+		}
+		if info, err := os.Stat(dir); err == nil && info.IsDir() {
+			dirChan <- dir
+		}
+	}
+	close(dirChan)
+}
+
+// shouldIgnoreDir checks if a directory should be ignored
+func (app *App) shouldIgnoreDir(dir string) bool {
+	return (filepath.Base(dir) == ".config" && strings.Contains(dir, "gart")) || filepath.Base(dir) == ".local"
+}
+
+// workerCopyFiles is a worker function that copies files in a directory
+func (app *App) workerCopyFiles(dirChan <-chan string, wg *sync.WaitGroup, directoryPath, name string) {
+	defer wg.Done()
+	for dirPath := range dirChan {
+		app.copyFilesInDir(dirPath, directoryPath, name)
+	}
+}
+
+// copyFilesInDir copies all files in a directory to the storage folder
+func (app *App) copyFilesInDir(dirPath, directoryPath, name string) {
+	err := filepath.Walk(dirPath, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && info.Size() > 1024*1024 {
+			fmt.Printf("Ignored file due to size limit: %s\n", filePath)
+			return nil
+		}
+		return app.copyFile(filePath, directoryPath, name, info)
+	})
+	if err != nil {
+		fmt.Printf("Error walking directory: %v\n", err)
+	}
+}
+
+// copyFile copies a file to the storage folder
+func (app *App) copyFile(filePath, directoryPath, name string, info os.FileInfo) error {
+	relPath, _ := filepath.Rel(directoryPath, filePath)
+	destPath := filepath.Join(app.StorePath, name, relPath)
+	if info.IsDir() {
+		return os.MkdirAll(destPath, info.Mode())
+	}
+	return system.CopyFile(filePath, destPath)
+}
+
+// addSingleDotfile adds a single dotfile to the store
+func (app *App) addSingleDotfile(path, name string) {
+	cleanedPath := filepath.Clean(path)
+	fmt.Printf("Adding dotfile: %s\n", cleanedPath)
+
+	storePath := filepath.Join(app.StorePath, name)
+	fmt.Printf("Store path: %s\n", storePath)
+
+	err := system.CopyDirectory(cleanedPath, storePath)
+	if err != nil {
+		fmt.Printf("Error copying directory: %v\n", err)
+		return
+	}
+
+	app.updateConfig(name, cleanedPath)
+}
+
+// updateConfig updates the config file with the new dotfile
+func (app *App) updateConfig(name, path string) {
+	err := config.AddDotfileToConfig(app.ConfigFilePath, name, path)
+	if err != nil {
+		fmt.Printf("Error adding dotfile to config: %v\n", err)
+		return
+	}
+
+	if app.Config.Dotfiles == nil {
+		app.Config.Dotfiles = make(map[string]string)
+	}
+	app.Config.Dotfiles[name] = path
+
+	fmt.Printf("Dotfile added: %s\n", name)
 }
