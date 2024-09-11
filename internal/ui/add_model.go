0d617bb36eeb952cbcb61e28a03f7c0f201aa0bc
bamen <hi@bamen.dev>
2024-09-11 08:21:14 +0200

    Add cmd: simplify print statements

--- internal/ui/add_model.go
+++ internal/ui/add_model.go
@@ -11,34 +11,28 @@ func RunAddDotfileView(app *app.App, path string, dotfileName string) {
 	path = app.ExpandHomeDir(path)
 	cleanedPath := filepath.Clean(path)
 
+	fmt.Printf("Adding dotfile %s... ", dotfileName)
+
 	var err error
 	if app.IsDir(path) {
 		err = addDotfileDir(app, cleanedPath, dotfileName)
-		if err != nil {
-			fmt.Println(errorStyle.Render("Error:"), err)
-			return
-		}
 	} else {
 		err = addDotfileFile(app, cleanedPath, dotfileName)
-		if err != nil {
-			fmt.Println(errorStyle.Render("Error:"), err)
-			return
-		}
 	}
 
-	fmt.Println(boldStyle.Render("Adding dotfile:"), cleanedPath)
-
 	if err != nil {
-		fmt.Println(errorStyle.Render("Error:"), err)
+		fmt.Println(errorStyle.Render("Error!"))
+		fmt.Println(err)
 		return
 	}
 
 	if err := app.GitCommitChanges("Add", dotfileName); err != nil {
-		fmt.Println(errorStyle.Render("Error committing changes:"), err)
+		fmt.Println(errorStyle.Render("Error!"))
+		fmt.Println("Error committing changes:", err)
 		return
 	}
 
-	fmt.Println(successStyle.Render("Dotfile added successfully!"))
+	fmt.Println(successStyle.Render("Success!"))
 }
 
 func addDotfileDir(app *app.App, cleanedPath, dotfileName string) error {
